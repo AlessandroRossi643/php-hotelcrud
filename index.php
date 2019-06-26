20340a0624d2a36aad9b8dcb1c5a30281f6cb31e
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-06-26 21:53:35 +0200

    2. Creata tabella con la select di tutte le stanze

--- index.php
+++ index.php
@@ -1,22 +1,48 @@
-<?php include 'sqlconn.php' ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Db_Hotel</title>
+    <link rel="stylesheet" href="public/css/app.css">
   </head>
   <body>
-    <table class="table">
-      <thead>
-        <tr>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container">
+      <?php include 'sqlconn.php' ?>
+
+      <?php
+      $sql = "SELECT * FROM stanze";
+      $result = $connessione->query($sql);
+      if ($result && $result->num_rows > 0) {
+      ?>
+
+        <div class="table-responsive">
+          <table class="table">
+            <tr>
+              <td>ID</td>
+              <td>Numero Stanze</td>
+              <td>Piano</td>
+              <td>Numero letti</td>
+            </tr>
+
+        <?php while ($row=$result->fetch_assoc()) { ?>
+            <tr>
+              <td><?php echo $row['id'];?></td>
+              <td><?php echo $row['room_number'];?></td>
+              <td><?php echo $row['floor'];?></td>
+              <td><?php echo $row['beds'];?></td>
+            </tr> <?php
+          }
+        }
+        elseif ($result) {
+          echo "0 results";
+        }
+        else{
+          echo "query error";
+        }
+        $connessione-> close();
+        ?>
+          </table>
+        </div>
+    </div>
   </body>
 </html>
--- sqlconn.php
+++ sqlconn.php
@@ -1,16 +1,15 @@
 <?php
+
 $servername="localhost";
 $username="root";
 $password="root";
 $dbname="db_hotel";
 
 $connessione= new mysqli($servername,$username,$password,$dbname);
+
 if ($connessione && $connessione->connect_error) {
-  echo "CONNESSIONE FALLITA:" . $connessione->connect_error;
+  echo ("CONNESSIONE FALLITA:" . $connessione->connect_error);
   exit();
 }
-else{
-  echo "CONNESSIONE STABILITA";
-  exit();
-}
+
 ?>
